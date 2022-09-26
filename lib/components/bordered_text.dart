fd8361a03484c19d4bba1cc453d0ee16ba4de9b0
Tomohiro629 <radcell0101@gmail.com>
2022-09-26 13:33:18 +0900

    bordered_text_widget作成

---
+++ lib/components/bordered_text.dart
@@ -0,0 +1,29 @@
+import 'package:bordered_text/bordered_text.dart';
+import 'package:flutter/material.dart';
+
+class BorderedTextWidget extends StatelessWidget {
+  const BorderedTextWidget({
+    Key? key,
+    required this.storokeColor,
+    required this.label,
+  }) : super(key: key);
+  final Color storokeColor;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return BorderedText(
+      strokeWidth: 1.0,
+      strokeColor: storokeColor,
+      child: Text(
+        label,
+        style: TextStyle(
+          color: Colors.white.withOpacity(0.7),
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 1,
+          wordSpacing: 4,
+        ),
+      ),
+    );
+  }
+}
