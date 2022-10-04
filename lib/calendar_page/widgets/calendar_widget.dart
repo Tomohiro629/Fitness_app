3406d4f48befa7fe9a11231298992590702a3d5e
Tomohiro629 <radcell0101@gmail.com>
2022-10-04 10:35:00 +0900

    当日データがないと場合のセットボタン追加

--- lib/calendar_page/widgets/calendar_widget.dart
+++ lib/calendar_page/widgets/calendar_widget.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutterfire_ui/firestore.dart';
+import 'package:karaoke_app/selected_day_date_gate/selected_day_date_gate.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-import '../../entity/record.dart';
-import '../../record_page/record_controller.dart';
-import '../../record_page/record_page.dart';
-import '../../service/common_method.dart';
 import '../calendar_controller.dart';
 
 class CalendarWidget extends ConsumerWidget {
@@ -18,62 +14,45 @@ class CalendarWidget extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final calendarModel = ref.watch(calendarControllerProvider);
 
-    return FirestoreListView<Record>(
-        query: ref.watch(recordControllerProvider).recordQuery(DateTime.now()),
-        itemBuilder: (context, snapshot) {
-          final record = snapshot.data();
-          return TableCalendar(
-            focusedDay: calendarModel.focusDay,
-            firstDay: DateTime.utc(2022, 9, 1),
-            lastDay: DateTime.utc(2100, 1, 1),
-            locale: 'ja_jp',
-            selectedDayPredicate: (day) {
-              return isSameDay(calendarModel.focusDay, day);
-            },
-            calendarFormat: calendarModel.format,
-            availableCalendarFormats: const {
-              CalendarFormat.month: 'Month',
-              CalendarFormat.week: 'Week',
-            },
-            startingDayOfWeek: StartingDayOfWeek.sunday,
-            calendarStyle: const CalendarStyle(
-              todayTextStyle: TextStyle(color: Colors.yellow),
-              todayDecoration: BoxDecoration(
-                color: Color.fromARGB(150, 6, 105, 186),
-                shape: BoxShape.circle,
-              ),
-              weekendTextStyle:
-                  TextStyle(color: Color.fromARGB(255, 255, 22, 22)),
-            ),
-            onFormatChanged: (format) {
-              ref
-                  .read(calendarControllerProvider.notifier)
-                  .changeFormat(format);
-            },
-            onDaySelected: (selectedDay, _) async {
-              ref
-                  .read(calendarControllerProvider.notifier)
-                  .changeDay(selectedDay);
+    return TableCalendar(
+      focusedDay: calendarModel.focusDay,
+      firstDay: DateTime.utc(2022, 9, 1),
+      lastDay: DateTime.utc(2100, 1, 1),
+      locale: 'ja_jp',
+      selectedDayPredicate: (day) {
+        return isSameDay(calendarModel.focusDay, day);
+      },
+      calendarFormat: calendarModel.format,
+      availableCalendarFormats: const {
+        CalendarFormat.month: 'Month',
+        CalendarFormat.week: 'Week',
+      },
+      startingDayOfWeek: StartingDayOfWeek.sunday,
+      calendarStyle: const CalendarStyle(
+        todayTextStyle: TextStyle(color: Colors.yellow),
+        todayDecoration: BoxDecoration(
+          color: Color.fromARGB(150, 6, 105, 186),
+          shape: BoxShape.circle,
+        ),
+        weekendTextStyle: TextStyle(color: Color.fromARGB(255, 255, 22, 22)),
+      ),
+      onFormatChanged: (format) {
+        ref.read(calendarControllerProvider.notifier).changeFormat(format);
+      },
+      onDaySelected: (selectedDay, _) async {
+        ref.read(calendarControllerProvider.notifier).changeDay(selectedDay);
 
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (
-                  context,
-                ) =>
-                        RecordPage(
-                          selectedDay: selectedDay,
-                        )),
-              );
-              if (getDateString(selectedDay) == getDateString(DateTime.now())) {
-                ref.read(recordControllerProvider).setDailyRecord(
-                    totalCalorie: record.totalCalorie,
-                    setCalorie: record.setCalorie,
-                    totalProtein: record.totalProtein,
-                    setProtein: record.setProtein);
-              }
-            },
-          );
-        });
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (
+            context,
+          ) =>
+                  SelectedDayDateGatePage(
+                    selectedDay: selectedDay,
+                  )),
+        );
+      },
+    );
   }
 }
---
+++ lib/selected_day_date_gate/selected_day_date_gate.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../entity/record.dart';
+import '../record_page/record_page.dart';
+import '../repository/record_repository.dart';
+import '../set_selected_day_date/set_selected_day_date_page.dart';
+
+class SelectedDayDateGatePage extends ConsumerWidget {
+  const SelectedDayDateGatePage({Key? key, required this.selectedDay})
+      : super(key: key);
+  final DateTime selectedDay;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedDayDate =
+        ref.watch(recordRepositoryProvider).fetchRecord(selectedDay);
+
+    return StreamBuilder<Record?>(
+        stream: selectedDayDate,
+        builder: ((context, snapshot) {
+          final isDate = snapshot.data != null;
+          return isDate
+              ? RecordPage(selectedDay: selectedDay)
+              : SetSelectedDayDatePage(selectedDay: selectedDay);
+        }));
+  }
+}
