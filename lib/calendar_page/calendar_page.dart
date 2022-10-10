f31af5ec19ae9083afa06566a5e1a734a973026b
Tomohiro629 <radcell0101@gmail.com>
2022-10-10 12:28:42 +0900

    カレンダーページのUI変更

--- lib/calendar_page/calendar_page.dart
+++ lib/calendar_page/calendar_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:karaoke_app/components/sfradial_gauge_widget.dart';
 import 'package:karaoke_app/user_edit_page/user_edit_page.dart';
 
 import '../components/bordered_text.dart';
@@ -15,30 +17,80 @@ class CalendarPage extends ConsumerWidget {
     return Scaffold(
       backgroundColor: const Color(0xff192028),
       body: SafeArea(
-          child: Column(children: [
-        Container(
-            padding: const EdgeInsets.all(50),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const BorderedTextWidget(
-                    label: "RECORD", storokeColor: Colors.indigo),
-                IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: ((context) => UserEditPage(
-                                    user: user,
-                                  ))));
-                    },
-                    icon: const Icon(Icons.settings))
-              ],
-            )),
-        Container(
-            padding: const EdgeInsets.only(top: 30.0, bottom: 150.0),
-            child: CalendarWidget(user: user)),
-      ])),
+          child: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+              padding: const EdgeInsets.all(30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Gap(20),
+                  const BorderedTextWidget(
+                      label: "RECORD", storokeColor: Colors.indigo),
+                  IconButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => UserEditPage(
+                                      user: user,
+                                    ))));
+                      },
+                      icon: const Icon(
+                        Icons.settings,
+                      ))
+                ],
+              )),
+          CalendarWidget(user: user),
+          Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: 350.0,
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        height: 300.0,
+                        width: 500.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        height: 300.0,
+                        width: 500.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        height: 300.0,
+                        width: 500.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add_a_photo_outlined,
+                      color: Color.fromARGB(255, 184, 13, 1),
+                    )),
+              ),
+            ],
+          )
+        ]),
+      )),
     );
   }
 }
